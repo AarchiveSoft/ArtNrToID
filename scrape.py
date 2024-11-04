1d3f01b061e738d797e3450cec6796669ee8900d
AaronTheGenerous <aaron@windowslive.com>
2024-11-04 11:27:19 +0100

    Add detailed docstrings to functions and reorganize code
    Enhanced code documentation by adding comprehensive docstrings to functions, providing clear explanations of their purpose, arguments, and return values. Additionally, refactored parts of the code for better readability and maintainability, including the use of context managers for database connections and the introduction of a query_database function for cleanly handling database queries.

--- scrape.py
+++ scrape.py
@@ -21,27 +21,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 email = "feigelluck@gmail.com"
 password = "Graphicart#1"
 
+
 def resource_path(relative_path):
+    """
+    Resource path utility function.
+
+    This function returns the absolute path of a resource file based on whether the application is running as a frozen executable or in a normal environment.
+
+    Args:
+        relative_path (str): The relative path of the resource file.
+
+    Returns:
+        str: The absolute path of the resource file.
+    """
     if getattr(sys, 'frozen', False):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.abspath(relative_path)
 
+
 def setup_db():
-    conn = sqlite3.connect("GambioIDs.db")
-    c = conn.cursor()
-    c.execute(
-        """
-        CREATE TABLE IF NOT EXISTS gambioIDs (
-        gambioID INTEGER PRIMARY KEY,
-        bezeichnung TEXT,
-        artNr TEXT
+    """
+    Set up the database schema if it does not already exist.
+
+    This function creates a SQLite database and a table for storing Gambio IDs if they are not already present.
+
+    Returns:
+        None
+    """
+    with sqlite3.connect("GambioIDs.db") as conn:
+        c = conn.cursor()
+        c.execute(
+            """
+            CREATE TABLE IF NOT EXISTS gambioIDs (
+            gambioID INTEGER PRIMARY KEY,
+            bezeichnung TEXT,
+            artNr TEXT
+            )
+            """
         )
-        """
-    )
-    conn.commit()
-    conn.close()
+        conn.commit()
+
 
 class GUI(QWidget):
+    """
+    GUI class for the Gambio ID Converter application.
+
+    This class initializes the user interface for the application, allowing users to select brands and categories, exclude articles, and convert article numbers to Gambio IDs.
+
+    Args:
+        scrape: An instance of the Scrape class used for web scraping operations.
+    """
+
     def __init__(self, scrape):
         super().__init__()
         self.title = "Gambio ID Konverter"
@@ -53,46 +83,57 @@ class GUI(QWidget):
         self.scraper = scrape
 
         self.category_data = {
-            "Kamerasysteme + Objektive": {
-                "Nikon": {
-                    "Nikon Z": {"initials": "NIVOA"},
-                    "Nikkor Z-Mount Objektive": {"initials": "NIJMA"},
-                    "Nikon DSLR": {"initials": "NIVBA"},
-                    "Nikkor F-Mount Objektive": {"initials": "NIJAA"},
-                    "Nikon Blitzgeräte": {"initials": "NIFSA"},
-                    "Nikon Coolpix": {"initials": "NIVQA"}
+            "Kamerasysteme + Objektive"         : {
+                "Nikon"                         : {
+                    "Nikon Z"                   : {"initials": "NIVOA"},
+                    "Nikkor Z-Mount Objektive"  : {"initials": "NIJMA"},
+                    "Nikon DSLR"                : {"initials": "NIVBA"},
+                    "Nikkor F-Mount Objektive"  : {"initials": "NIJAA"},
+                    "Nikon Blitzgeräte"         : {"initials": "NIFSA"},
+                    "Nikon Coolpix"             : {"initials": "NIVQA"}
                 },
-                "Sony": {
-                    "Sony E-Mount Kameras": {"initials": "SOILCE"},
-                    "Sony E-Mount Objektive": {"initials": "SOSEL"},
-                    "Sony Kompaktkameras": {"initials": "SOZV & SODSC"}
+                "Sony"                          : {
+                    "Sony E-Mount Kameras"      : {"initials": "SOILCE"},
+                    "Sony E-Mount Objektive"    : {"initials": "SOSEL"},
+                    "Sony Kompaktkameras"       : {"initials": "SOZV & SODSC"}
                 },
-                "Fujifilm": {
-                    "Fujifilm GFX Kameras": {"initials": "FJ"},
-                    "Fujifilm GFX Objektive": {"initials": "FJ"},
-                    "Fujifilm X Kameras": {"initials": "FJ"}
+                "Fujifilm"                      : {
+                    "Fujifilm GFX Kameras"      : {"initials": "FJ"},
+                    "Fujifilm GFX Objektive"    : {"initials": "FJ"},
+                    "Fujifilm X Kameras"        : {"initials": "FJ"}
                 },
-                "Phase One": {
-                    "Phase One IQ Backs": {"initials": "PO"},
+                "Phase One"                     : {
+                    "Phase One IQ Backs"        : {"initials": "PO"},
                     "Phase One XF Camera System": {"initials": "PO"}
                 },
-                "Cambo": {
-                    "Cambo Wide RS": {"initials": "CA"},
-                    "Cambo ACTUS": {"initials": "CA"}
+                "Cambo"                         : {
+                    "Cambo Wide RS"             : {"initials": "CA"},
+                    "Cambo ACTUS"               : {"initials": "CA"}
                 },
-                "Leica": {
-                    "Leica M & Objektive": {"initials": "n/a"},
-                    "Leica Q": {"initials": "n/a"}
+                "Leica"                         : {
+                    "Leica M & Objektive"       : {"initials": "n/a"},
+                    "Leica Q"                   : {"initials": "n/a"}
                 }
             }
         }
 
+        self.main_icon_path = resource_path('main_icon.ico')
+        self.loading_icon_path = resource_path('loading_icon.ico')
+
         self.initUI()
 
     def initUI(self):
+        """
+        Initialize the user interface components.
+
+        This method sets up the main layout, buttons, dropdowns, and connects signals to their respective slots.
+
+        Returns:
+            None
+        """
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setWindowIcon(QIcon(resource_path('main_icon.ico')))
+        self.setWindowIcon(QIcon(self.main_icon_path))
 
         main_layout = QVBoxLayout()
 
@@ -147,49 +188,107 @@ class GUI(QWidget):
         self.show()
 
     def copy_to_clipboard(self):
+        """
+        Copy the text from the result textbox to the clipboard.
+
+        This method retrieves the text from the result display area and sets it to the system clipboard.
+
+        Returns:
+            None
+        """
         clipboard = QApplication.clipboard()
         clipboard.setText(self.result_textbox.toPlainText())
 
     def change_format(self):
+        """
+        Change the format of the text in the result textbox.
+
+        This method processes the current text, removing empty lines and formatting it into a comma-separated string.
+
+        Returns:
+            None
+        """
         current_text = self.result_textbox.toPlainText()
-        formatted_text = ", ".join(filter(None, current_text.split("\n")))
+        formatted_text = ", ".join([line for line in current_text.split("\n") if line])
         self.result_textbox.setPlainText(formatted_text)
 
     def update_subcategories(self):
+        """
+        Update the category dropdown based on the selected brand.
+
+        This method clears the current categories and populates the dropdown with subcategories corresponding to the selected brand.
+
+        Returns:
+            None
+        """
         selected_brand = self.marken_combobox.currentText()
         self.kategorie_combobox.clear()
         if selected_brand in self.category_data["Kamerasysteme + Objektive"]:
             self.kategorie_combobox.addItems(self.category_data["Kamerasysteme + Objektive"][selected_brand].keys())
 
     def convert_articles(self):
+        """
+        Convert selected articles to Gambio IDs.
+
+        This method retrieves the selected brand and category, processes excluded articles, and queries the database for matching Gambio IDs.
+
+        Returns:
+            None
+        """
+
         selected_brand = self.marken_combobox.currentText()
         selected_category = self.kategorie_combobox.currentText()
         excluded_articles = [article.strip() for article in self.exclude_input.text().split(",") if article.strip()]
         initials = self.category_data["Kamerasysteme + Objektive"][selected_brand][selected_category].get("initials")
 
         if initials == "n/a":
-            QMessageBox.warning(self, "Kategorie nicht verwendbar", "Die ausgewählte Kategorie kann nicht verwendet werden.")
+            QMessageBox.warning(self, "Kategorie nicht verwendbar",
+                                "Die ausgewählte Kategorie kann nicht verwendet werden.")
             return
 
         try:
             with sqlite3.connect("GambioIDs.db") as conn:
-                c = conn.cursor()
-                query = "SELECT gambioID FROM gambioIDs WHERE artNr LIKE ?"
-                parameters = [f"{initials}%"]
-
-                if excluded_articles:
-                    placeholders = ", ".join("?" for _ in excluded_articles)
-                    query += f" AND artNr NOT IN ({placeholders})"
-                    parameters.extend(excluded_articles)
-
-                c.execute(query, parameters)
-                gambio_ids = [str(row[0]) for row in c.fetchall()]
-                self.result_textbox.setText("\n".join(gambio_ids))
+                self.query_database(conn, initials, excluded_articles)
         except Exception as e:
             QMessageBox.critical(self, "Fehler", f"An error occurred when pulling from DB: {e}")
 
+    def query_database(self, conn, initials, excluded_articles):
+        """
+        Query the database for Gambio IDs based on the selected criteria.
+
+        This method executes a SQL query to retrieve Gambio IDs while considering excluded articles.
+
+        Args:
+            conn: The database connection object.
+            initials (str): The initials to filter Gambio IDs.
+            excluded_articles (list): A list of articles to exclude from the results.
+
+        Returns:
+            None
+        """
+        c = conn.cursor()
+        query = "SELECT gambioID FROM gambioIDs WHERE artNr LIKE ?"
+        parameters = [f"{initials}%"]
+
+        if excluded_articles:
+            placeholders = ", ".join("?" for _ in excluded_articles)
+            query += f" AND artNr NOT IN ({placeholders})"
+            parameters.extend(excluded_articles)
+
+        c.execute(query, parameters)
+        gambio_ids = [str(row[0]) for row in c.fetchall()]
+        self.result_textbox.setText("\n".join(gambio_ids))
+
     def open_progress_window(self):
-        self.progress_window = ProgressWindow()
+        """
+        Open the progress window for scraping operations.
+
+        This method initializes and displays the progress window, connecting it to the scraping thread for updates.
+
+        Returns:
+            None
+        """
+        self.progress_window = ProgressWindow(self.loading_icon_path)  # Pass the icon path
         self.progress_window.show()
         self.scrape_thread = ScrapeThread(self.scraper)
         self.scrape_thread.progress_updated.connect(self.progress_window.progress_bar.setValue)
@@ -198,16 +297,47 @@ class GUI(QWidget):
         self.scrape_thread.start()
 
     def show_error_message(self, error_message):
+        """
+        Display an error message in a message box.
+
+        This method shows a critical error message dialog with the provided error message.
+
+        Args:
+            error_message (str): The error message to display.
+
+        Returns:
+            None
+        """
         QMessageBox.critical(self, "Fehler", error_message)
 
 
 class ProgressWindow(QDialog):
-    def __init__(self):
+    """
+    Progress window for displaying scraping progress.
+
+    This class creates a dialog that shows the progress of the scraping operation, including a progress bar and completion messages.
+
+    Args:
+        loading_icon_path (str): The path to the loading icon to be displayed in the window.
+    """
+
+    def __init__(self, loading_icon_path):
         super().__init__()
+        self.loading_icon_path = loading_icon_path  # Store the icon path
         self.initUI()
-        self.setWindowIcon(QIcon(resource_path('loading_icon.ico')))
+        self.setWindowIcon(QIcon(self.loading_icon_path))
+
+        self.center_on_right_half()
 
     def initUI(self):
+        """
+        Initialize the user interface components for the progress window.
+
+        This method sets up the progress bar and its layout.
+
+        Returns:
+            None
+        """
         self.setWindowTitle("Scraping Progress")
         self.setFixedSize(400, 200)
         self.progress_bar = QProgressBar(self)
@@ -231,18 +361,54 @@ class ProgressWindow(QDialog):
         self.layout.addWidget(self.progress_bar)
         self.setLayout(self.layout)
 
+    def center_on_right_half(self):
+        """
+        Center the progress window in the middle of the right half of the screen.
+
+        This method calculates the appropriate position for the window and moves it accordingly.
+
+        Returns:
+            None
+        """
+        screen = QApplication.primaryScreen()  # Get the primary screen
+        screen_geometry = screen.geometry()  # Get the geometry of the screen
+        right_half_width = screen_geometry.width() // 2  # Width of the right half
+        x = right_half_width + (right_half_width - self.width()) // 2  # Center in the right half
+        y = (screen_geometry.height() - self.height()) // 2  # Center vertically
+        self.move(x, y)  # Move to the calculated position
+
     def show_completion_message(self):
+        """
+        Display a completion message when the scraping is finished.
+
+        This method hides the progress bar and shows a message indicating that the database has been updated.
+
+        Returns:
+            None
+        """
         self.progress_bar.hide()
         completion_label = QLabel("Datenbank aktualisiert", self)
         completion_label.setStyleSheet("font-size: 20px; font-weight: bold; color: green;")
         completion_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(completion_label)
+
+        # Add a close button
+        close_button = QPushButton("Schliessen", self)
+        close_button.clicked.connect(self.close)  # Connect the 'clicked' signal to the window's close slot
+        self.layout.addWidget(close_button)
+
         self.adjustSize()
-        time.sleep(2)
-        self.close()
 
 
 class ScrapeThread(QThread):
+    """
+    Thread for performing scraping operations.
+
+    This class handles the execution of scraping tasks in a separate thread, allowing the GUI to remain responsive during long-running operations.
+
+    Args:
+        scraper: An instance of the Scrape class used for web scraping operations.
+    """
     progress_updated = pyqtSignal(int)
     scraping_completed = pyqtSignal()
     error_occurred = pyqtSignal(str)
@@ -252,6 +418,14 @@ class ScrapeThread(QThread):
         self.scraper = scraper
 
     def run(self):
+        """
+        Execute the scraping operations.
+
+        This method runs the scraping process, including setting up the driver, logging in, and navigating to the target page.
+
+        Returns:
+            None
+        """
         try:
             self.scraper.setup_driver()
             self.scraper.login(email, password)
@@ -262,20 +436,62 @@ class ScrapeThread(QThread):
 
 
 class Scrape:
+    """
+    Scrape class for handling web scraping operations.
+
+    This class manages the setup of the web driver, login process, and data retrieval from the target website.
+
+    Args:
+        None
+    """
     def setup_driver(self):
+        """
+        Set up the web driver for scraping.
+
+        This method initializes the Chrome web driver with the appropriate options and navigates to the target website.
+
+        Returns:
+            None
+        """
         base_path = sys._MEIPASS if getattr(sys, "frozen", False) else os.path.dirname(os.path.abspath(__file__))
         chromedriver_path = os.path.join(base_path, 'chromedriver.exe')
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.path.join(base_path, 'chrome', 'win64-118.0.5993.70', 'chrome-win64', 'chrome.exe')
+        chrome_options.binary_location = os.path.join(base_path, 'chrome', 'win64-118.0.5993.70', 'chrome-win64',
+                                                      'chrome.exe')
         service = Service(chromedriver_path)
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
         self.driver.get("https://www.graphicart.ch/shop/de/")
 
     def wait(self, condition, time=10):
+        """
+        Wait for a specific condition to be met.
+
+        This method blocks execution until the specified condition is met or the timeout occurs.
+
+        Args:
+            condition: The condition to wait for.
+            time (int): The maximum time to wait in seconds.
+
+        Returns:
+            The result of the condition when it is met.
+        """
         return WebDriverWait(self.driver, time).until(condition)
 
     def login(self, email, password):
-        kundenlogin_button = self.wait(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.dropdown-toggle[title="Anmeldung"]')))
+        """
+        Perform login to the target website.
+
+        This method interacts with the web elements to enter the email and password, and submits the login form.
+
+        Args:
+            email (str): The email address for login.
+            password (str): The password for login.
+
+        Returns:
+            None
+        """
+        kundenlogin_button = self.wait(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'a.dropdown-toggle[title="Anmeldung"]')))
         kundenlogin_button.click()
         email_field = self.wait(EC.presence_of_element_located((By.ID, 'box-login-dropdown-login-username')))
         email_field.send_keys(email)
@@ -285,11 +501,23 @@ class Scrape:
         login_button.click()
 
     def navigate_gambio(self, progress_signal):
+        """
+        Navigate to the Gambio admin page and scrape data.
+
+        This method retrieves product data from the Gambio admin page and emits progress updates.
+
+        Args:
+            progress_signal: A signal to update the progress bar.
+
+        Returns:
+            None
+        """
         with sqlite3.connect("GambioIDs.db") as conn:
             c = conn.cursor()
             self.driver.get("https://www.graphicart.ch/shop/admin/validproducts.php")
             self.wait(EC.visibility_of_element_located((By.CSS_SELECTOR, ".pageHeading")))
-            list_container = self.wait(EC.visibility_of_element_located((By.CSS_SELECTOR, "body > table:nth-child(1) > tbody:nth-child(1)")))
+            list_container = self.wait(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, "body > table:nth-child(1) > tbody:nth-child(1)")))
             rows = list_container.find_elements(By.CSS_SELECTOR, "tr")[2:]
             total_rows = len(rows)
 
@@ -302,7 +530,8 @@ class Scrape:
                 gambio_id = columns[0].text
                 art_name = columns[1].text
                 art_nr = columns[2].text
-                c.execute("INSERT OR IGNORE INTO gambioIDs (gambioID, bezeichnung, artNr) VALUES (?, ?, ?)", (gambio_id, art_name, art_nr))
+                c.execute("INSERT OR IGNORE INTO gambioIDs (gambioID, bezeichnung, artNr) VALUES (?, ?, ?)",
+                          (gambio_id, art_name, art_nr))
                 if index % 10 == 0 or index == total_rows - 1:
                     conn.commit()
                 progress_percentage = int((index + 1) / total_rows * 100)
@@ -310,6 +539,7 @@ class Scrape:
 
             conn.commit()
             self.driver.quit()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
